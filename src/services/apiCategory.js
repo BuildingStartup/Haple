4228b79ab31e1037dd07d5246dd876dedeca9c10
Daniel King <dking9486@gmail.com>
2026-02-20 22:09:32 +0100

    categories api

--- src/services/apiCategory.js
+++ src/services/apiCategory.js
@@ -1,4 +1,31 @@
-export async function getAllCategories(){};
-export async function getCategoryBySlug(slug){};
+import supabase from "./supabase";
+
+export async function getAllCategories(){
+    const {data: categories, error} = await supabase
+    .from("categories")
+    .select("*");
+    
+    if(error){
+        console.log(error);
+        throw new Error(error);
+    }
+
+    return categories;
+};
+
+export async function getCategoryBySlug(slug){
+    const {data: category, error} = await supabase
+    .from("categories")
+    .select("*")
+    .eq("slug", slug)
+    .single();
+    
+    if(error){
+        console.log(error);
+        throw new Error(error);
+    }
+
+    return category;
+};
 
 
