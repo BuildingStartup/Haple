cbf96adfad77825f699bd7fa186dc8fe5d6443cb
Joel Ohikhena <122086681+Joel1716@users.noreply.github.com>
2026-02-22 17:02:50 +0100

    Home Page Added (#1)
    * Home Page Added
    
    * UI fix
    
    * homepageUi new fix
    
    ---------
    
    Co-authored-by: Daniel King <dking9486@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,39 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-
+// PAGES
+import Home from "./pages/Home.jsx";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        {/* routes go in here */}
-      </Routes>
-    </BrowserRouter>
-    <Toaster 
-    position="top-center"
-    gutter={12}
-    containerStyle={{margin: "8px"}}
-    toastOptions={{
-      success: {
-        duration: 3000,
-      },
-      error: {
-        duration: 5000,
-      },
-      style: {
-        fontSize: "16px",
-        maxWidth: "500px",
-        padding: "16px 20px",
-        backgroundColor: "white",
-        color: "black"
-      }
-    }}
-    />
+      <BrowserRouter>
+        <Routes>
+          {/* routes go in here */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 20px",
+            backgroundColor: "white",
+            color: "black",
+          },
+        }}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
