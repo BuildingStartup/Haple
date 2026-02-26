7b4f94702cea52dddf6ea5aacfca8d01883b5471
Daniel King <dking9486@gmail.com>
2026-02-26 22:59:52 +0100

    profile ui update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -16,13 +16,13 @@ export default function Profile() {
       image: "https://placehold.co/200x150",
     },
   ];
+
   const sellerInfo = {
     business_name: "Chunkz",
     description:
       "GitHub users are now required to enable two-factor authentication as an additional security measure. Your activity on GitHub includes you in this requirement. You will need to enable two-factor authentication on your account before April 07, 2026, or be restricted from account actions.",
     whatsapp_number: "+2348135503380",
-    email: "jayzeeohiozoje@gmail.com",
-    categories: "Food",
+    categories: "Clothes",
     mode: "product",
   };
   const whatsappNumber = sellerInfo.whatsapp_number.replace(/\D/g, "");
@@ -30,20 +30,19 @@ export default function Profile() {
     `Hi ${sellerInfo.business_name}, I found you on Haple and I'm interested in your products!`
   );
   return (
-    <section className="min-h-screen space-y-3">
-      {/* Navigate so when you go back, it doesn't remount the sellers page and
-      the filter still has it's effect */}
-      <div>
+    <section className="min-h-screen space-y-5">
+      
         <div className="bg-primary p-5 relative h-45 mb-30">
+
           <div className="flex justify-between items-center">
             <button
               onClick={() => navigate(-1)}
-              className="flex items-center gap-2 cursor-pointer"
-            >
+              className="flex items-center gap-2 cursor-pointer">
               <GoArrowLeft className="text-2xl text-stone-100" />
               <span className="text-stone-100">Back</span>
             </button>
           </div>
+
           {/* Seller Info */}
           <div className=" flex flex-col gap-3 items-center absolute -bottom-25 left-0 right-0 mx-auto">
             {/* Avatar */}
@@ -53,7 +52,9 @@ export default function Profile() {
               </span>
             </div>
 
-            <h2 className="text-2xl font-medium">{sellerInfo.business_name}</h2>
+            <h2 className="text-2xl font-medium">
+              {sellerInfo.business_name}
+            </h2>
 
             <div className="">
               <span className="bg-stone-100 rounded-full py-1 px-4 capitalize">
@@ -61,19 +62,22 @@ export default function Profile() {
               </span>
             </div>
           </div>
+
         </div>
 
         {/* Description */}
         <div className="px-5 pt-2 text-stone-700 leading-loose">
           {sellerInfo.description}
         </div>
-      </div>
+
+
       {/* Catalog Text */}
       <div className="flex items-center gap-3 mb-3">
         <div className="flex-1 h-px bg-stone-200" />
-        <h3 className="text-stone-400 tracking-widest">Catalog</h3>
+          <h3 className="text-stone-400 tracking-widest">Catalog</h3>
         <div className="flex-1 h-px bg-stone-200" />
       </div>
+
       {/* How it looks like when added */}
       <div className="flex flex-wrap gap-3 p-5">
         {products.map((prod, i) => (
@@ -90,19 +94,21 @@ export default function Profile() {
           </div>
         ))}
       </div>
+
       {/* Contact Row */}
-      <div className="px-10 mb-5">
+      <div className="p-5">
         <a
           href={`https://wa.me/${whatsappNumber}?text=${message}`}
           target="_blank"
           rel="noreferrer"
         >
-          <button className="bg-[#25D366] w-full text-white py-3 text-base rounded-md flex items-center justify-center gap-2 cursor-pointer">
+          <button className="bg-secondary w-full text-white py-3 text-base rounded flex items-center justify-center gap-2 cursor-pointer">
             <FaWhatsapp className="text-xl" />
             Message on Whatsapp
           </button>
         </a>
       </div>
+
     </section>
   );
 }
