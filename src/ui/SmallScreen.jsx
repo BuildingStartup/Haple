bdf805ea7bdbf6ff326a268fcc9c2121b715ad89
Joel Ohikhena <joelohikhena12@gmail.com>
2026-03-08 22:31:47 +0100

    Splash Screen added

--- src/ui/SmallScreen.jsx
+++ src/ui/SmallScreen.jsx
@@ -1,7 +1,18 @@
-export default function SmallScreen({ children }){
-    return (
-        <div className="block md:hidden">
-            {children}
-        </div>
-    )
+import { useEffect, useState } from "react";
+import SplashScreen from "./SplashScreen";
+import { AnimatePresence } from "framer-motion";
+export default function SmallScreen({ children }) {
+  const [splash, setSplash] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSplash(false);
+    }, 2500);
+    return () => clearTimeout(timeout);
+  }, []);
+  return (
+    <>
+      <AnimatePresence>{splash && <SplashScreen />}</AnimatePresence>
+      <div className="block md:hidden">{children}</div>
+    </>
+  );
 }
